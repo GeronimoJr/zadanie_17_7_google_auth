f6cc343f1785197c25e9945237e6c55de9369fa1
Arnold Sztandarski <sztandarski.arnold@gmail.com>
2018-02-20 12:58:06 +0100

    Add files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '624513654642-5k1dpns2330vlqphvcj5fktbs672jtqi.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '1nLi9sWImmmgszSf2fcCvwxV',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
